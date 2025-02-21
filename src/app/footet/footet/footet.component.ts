f0c2eab9a04c6ccb733cd28a4931b5ed895b66aa
Miguel E. Cabrera <mecg1994@gmail.com>
2025-02-20 23:41:58 -0600

    footer

--- src/app/footet/footet/footet.component.ts
+++ src/app/footet/footet/footet.component.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './footet.component.css'
 })
 export class FootetComponent {
-
+  showModal = false;
+  
+  copyEmail() {
+    navigator.clipboard.writeText('mecg1994@gmail.com')
+      .then(() => {
+        alert('Email copiado al portapapeles');
+      })
+      .catch(err => {
+        console.error('Error al copiar el email:', err);
+      });
+  }
 }
